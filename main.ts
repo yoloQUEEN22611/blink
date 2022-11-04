f642ee1e5c01da92eefad135f9be77ca85aef98f
yoloQUEEN22611 <117457326+yoloQUEEN22611@users.noreply.github.com>
2022-11-04 09:37:56 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    basic.showString("Hello!")
+    basic.showString("...")
+    basic.showString("I'm Adah")
+    basic.showIcon(IconNames.Heart)
+})
